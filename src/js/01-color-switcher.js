5c2ab4f9998b2e925c6e14aee0e866085cbfb380
Yauheni Kaspiarovich <kaspiarovichy@gmail.com>
2022-05-11 20:28:45 +0200

    add color-switcher-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+let timeoutId = null;
 
+refs = {
+    btnStart: document.querySelector('[data-start]'),
+    btnStop: document.querySelector('[data-stop]')
+}
+
+
+refs.btnStart.addEventListener('click', onSwitcherStartClick);
+refs.btnStop.addEventListener('click', onSwitcherStopClick);
+
+function onSwitcherStartClick() {
+    if (refs.btnStop) {
+        refs.btnStart.disabled = true;
+    }
+    timeoutId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    },1000)
+}
+
+function onSwitcherStopClick() {
+    clearInterval(timeoutId);
+}
+
+
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
