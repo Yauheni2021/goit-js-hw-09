b701d52236d82e8b4d5920e16cb5ab12c2781e94
Yauheni Kaspiarovich <kaspiarovichy@gmail.com>
2022-05-13 15:34:26 +0200

    install notifix

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,5 +3,43 @@ import flatpickr from "flatpickr";
 // Дополнительный импорт стилей
 import "flatpickr/dist/flatpickr.min.css";
 
+import Notiflix from 'notiflix';
 // flatpickr("#datetime-picker", { dateFormat });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
